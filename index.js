197b4664f9a8c776be9faf64e9d210e49a820bf5
bkt44 <30497729+bkt44@users.noreply.github.com>
2019-03-31 17:12:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 'use strict';
-// test in /home/volumio/volumio-plugins/plugins/system_controller/rpi_gpio_action_buttons
-// volumio vrestart
-// volumio plugin refresh
-// sudo journalctl -f
-
 
 var libQ = require('kew');
 //var fs=require('fs-extra');
@@ -14,7 +9,6 @@ var Gpio = require('onoff').Gpio;
 var io = require('socket.io-client');
 var socket = io.connect('http://localhost:3000');
 var switches = ["SW1", "SW2", "SW3", "SW4", "SW5", "SW6", "SW7"];
-//var swval = [1,1,1,1,1,1,1];
 
 module.exports = rpiGpioActionButtons;
 function rpiGpioActionButtons(context) {
@@ -59,7 +53,6 @@ rpiGpioActionButtons.prototype.onStart = function() {
 rpiGpioActionButtons.prototype.onStop = function() {
     var self = this;
     var defer=libQ.defer();
-    // Once the Plugin has successfull stopped resolve the promise
 
 	self.clearTriggers()
 		.then (function (result) {
@@ -67,13 +60,11 @@ rpiGpioActionButtons.prototype.onStop = function() {
 			defer.resolve();
 		});
 
-    //return libQ.resolve();
 	return defer.promise;
 };
 
 rpiGpioActionButtons.prototype.onRestart = function() {
     var self = this;
-    // Optional, use if you need it
 };
 
 
@@ -168,12 +159,6 @@ rpiGpioActionButtons.prototype.saveConfig = function(data) {
 		self.config.set(c1, d1);
 		self.config.set(c2, d2);
 		self.config.set(c3, d3);
-		
-		//var s10=d3.service;
-		//var s11=d3.name;
-		//var s12=d3.uri;
-		//var s13=JSON.stringify(d3);
-		//self.logger.info('RPI-GPIO-Buttons-s: '+d1+' '+d2+' '+s10+' '+s11+' '+s12+' - '+s13);
 	});
 
 	self.clearTriggers()
@@ -194,11 +179,6 @@ rpiGpioActionButtons.prototype.createTriggers = function() {
 		if(enabled === true){
 			var c2 = action.concat('.pin');
 			var pin = self.config.get(c2);
-			//var c3 = action.concat('.value');
-			//var s7=self.config.get(c3);
-			//var vis=s7.service+' '+s7.name;
-			//self.logger.info('RPI-GPIO-Buttons: '+ action + ' on GPIO' + pin+' S: '+vis+' index='+index);
-			
 			var jj = new Gpio(pin,'in','falling', {debounceTimeout: 200}); // 
 			jj.watch(self.listener.bind(self,index+1));
 			self.triggers.push(jj);
@@ -213,7 +193,6 @@ rpiGpioActionButtons.prototype.clearTriggers = function () {
 	var self = this;
 	self.logger.info("RPI-GPIO-Buttons: Destroying triggers:");
 	self.triggers.forEach(function(trigger, index, array) {
-  		//self.logger.info("RPI-GPIO-Buttons: Destroying trigger " + index);
 		trigger.unwatchAll();
 		trigger.unexport();		
 
@@ -226,7 +205,6 @@ rpiGpioActionButtons.prototype.clearTriggers = function () {
 
 rpiGpioActionButtons.prototype.listener = function(action,err,value){
 	var self = this;
-	//self.logger.info('RPI-GPIO-Buttons SW'+action+' Err: '+err+' Value: '+value);
 	var c3 = 'SW'+action+'.value';
 	var s7=self.config.get(c3);
 	if (s7.service == "volumio") { 
@@ -270,17 +248,14 @@ rpiGpioActionButtons.prototype.listener = function(action,err,value){
 
 rpiGpioActionButtons.prototype.setUIConfig = function(data) {
 	var self = this;
-	//Perform your installation tasks here
 };
 
 rpiGpioActionButtons.prototype.getConf = function(varName) {
 	var self = this;
-	//Perform your installation tasks here
 };
 
 rpiGpioActionButtons.prototype.setConf = function(varName, varValue) {
 	var self = this;
-	//Perform your installation tasks here
 };
 
 
